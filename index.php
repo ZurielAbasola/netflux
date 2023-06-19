85b56cc59bebe65b904b8515ab15ee1be7b26ce5
ZurielAbasola <zuriel123.abasola@gmail.com>
2023-06-18 18:48:53 -0600

    PreviewProvider class

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 require_once("includes/config.php");
+require_once("includes/classes/PreviewProvider.php");
 
 if(!isset($_SESSION["userLoggedIn"])) {
     header("Location: register.php");
 }
-?>
 
-Hello! Work in progress...
+$userLoggedIn = $_SESSION["userLoggedIn"];
+
+$preview = new PreviewProvider($con, $userLoggedIn);
+echo $preview->createPreviewVideo();
+?>
