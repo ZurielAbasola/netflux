c472d045c9ce817b79d6bd69836a7bcdabd66cca
ZurielAbasola <zuriel123.abasola@gmail.com>
2023-08-02 15:07:14 -0600

    initial commit

--- includes/classes/Video.php
+++ includes/classes/Video.php
@@ -4,19 +4,19 @@ class Video {
 
     public function __construct($con, $input) {
         $this->con = $con;
-    }
 
-    if(is_array($input)) {
-        $this->sqlData = $input;
-    }
-    else {
-        $query = $this->con->prepare("SELECT * FROM videos WHERE id=:id");
-        $query->bindValue(":id", $input);
-        $query->execute();
+        if(is_array($input)) {
+            $this->sqlData = $input;
+        }
+        else {
+            $query = $this->con->prepare("SELECT * FROM videos WHERE id=:id");
+            $query->bindValue(":id", $input);
+            $query->execute();
 
-        $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
-    }
+            $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
+        }
 
-    $this->entity = new Entity($con, $this->sqlData["entityId"]);
+        $this->entity = new Entity($con, $this->sqlData["entityId"]);
+    }
 }
 ?>
