7cf7576112e91464bf86ab67598f642999413a3e
ZurielAbasola <zuriel123.abasola@gmail.com>
2023-08-15 15:15:34 -0600

    Video progress finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,5 +30,45 @@ function startHideTimer() {
 
 function initVideo(videoId, userLoggedIn) {
     startHideTimer();
-    
+    updateProgressTimer(videoId, userLoggedIn);
+}
+
+function updateProgressTimer(videoId, userLoggedIn) {
+    addDuration(videoId, userLoggedIn);
+
+    var timer;
+
+    $("video").on("playing", function(event) {
+        window.clearInterval(timer);
+        timer = window.setInterval(function() {
+            updateProgress(videoId, userLoggedIn, event.target.currentTime);
+        }, 3000);
+    }).on("ended", function() {
+        setFinished(videoId, userLoggedIn);
+        window.clearInterval(timer);
+    })
+}
+
+function addDuration(videoId, userLoggedIn) {
+    $.post("ajax/addDuration.php", { videoId: videoId, username: userLoggedIn }, function(data) {
+        if (data !== null && data !== "") {
+            alert(data);
+        }
+    })
+}
+
+function updateProgress(videoId, userLoggedIn, progress) {
+    $.post("ajax/updateDuration.php", { videoId: videoId, username: userLoggedIn, progress: progress }, function(data) {
+        if (data !== null && data !== "") {
+            alert(data);
+        }
+    })
+}
+
+function setFinished(videoId, userLoggedIn) {
+    $.post("ajax/setFinished.php", { videoId: videoId, username: userLoggedIn }, function(data) {
+        if (data !== null && data !== "") {
+            alert(data);
+        }
+    })
 }
