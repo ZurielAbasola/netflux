6c509828df4369627a81e9b10b669cd14ff9874a
ZurielAbasola <zuriel123.abasola@gmail.com>
2023-08-15 15:45:58 -0600

    Check next video

--- includes/classes/VideoProvider.php
+++ includes/classes/VideoProvider.php
@@ -12,6 +12,18 @@ class VideoProvider {
         $query->bindData(":videoId", $curentVideo->getId());
 
         $query->execute();
+
+        if ($query->rowCount() == 0) {
+            $query = $con->prepare("SELECT * FROM videos
+                                    WHERE season <= 1 AND episode <= 1
+                                    AND id != :videoId
+                                    ORDER BY views DESC LIMIT 1");
+            $query->bindData(":videoId", $curentVideo->getId());
+            $query->execute();
+        }
+
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+        return new Video($con, $row);
     }
 }
 ?>
