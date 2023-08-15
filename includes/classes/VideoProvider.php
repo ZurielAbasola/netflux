fa70f02420f4b50e0f0bc05a4a03f2110bfd740f
ZurielAbasola <zuriel123.abasola@gmail.com>
2023-08-15 15:42:43 -0600

    Get next episode

---
+++ includes/classes/VideoProvider.php
@@ -0,0 +1,17 @@
+<?php
+class VideoProvider {
+    public static function getUpNext ($con, $curentVideo) {
+        $query = $con->prepare("SELECT * FROM videos
+                                WHERE entityId = :entityId AND id != :videoId
+                                AND (
+                                    (season = :season AND episode > :episode) OR season > :season)
+                                ORDER BY season, episode ASC LIMIT 1");
+        $query->bindData(":entityId", $curentVideo->getEntityId());
+        $query->bindData(":season", $curentVideo->getSeasonNumber());
+        $query->bindData(":episode", $curentVideo->getEpisodeNumber());
+        $query->bindData(":videoId", $curentVideo->getId());
+
+        $query->execute();
+    }
+}
+?>
